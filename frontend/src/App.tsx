7db8509217cc26c78e3e7bec9c5c4b46637a7f94
drifterz13 <mixmyxxe@gmail.com>
2025-01-09 10:01:34 +0700

    chore: add example

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,16 @@ export default function App() {
     <div className="App">
       {/* <H5pPlayer h5pJsonPath="/assets/h5p/ex-1" /> */}
 
+      <h1>Video example: 1</h1>
       <H5pPlayer
         h5pJsonPath="http://localhost:3000/videos/ex-1"
         contentJsonPath="http://localhost:3000/videos/ex-1/content"
+      />
+
+      <h1>Video example: 2</h1>
+      <H5pPlayer
+        h5pJsonPath="http://localhost:3000/videos/ex-2"
+        contentJsonPath="http://localhost:3000/videos/ex-2/content"
       />
     </div>
   );
